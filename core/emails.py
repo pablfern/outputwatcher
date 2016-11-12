2eaef53e642e511f2a31e8c9b14b003f17896f1b
Pablo <pablfern@itba.edu.ar>
2016-11-12 19:32:23 -0300

    Adjust send_email method

--- core/emails.py
+++ core/emails.py
@@ -3,11 +3,10 @@ from django.conf import settings
 from django.core.mail import EmailMessage
 
 
-def send_email(subject, body, to_emails, **kwargs):
-    email = EmailMessage(subject,
-                         body,
+def send_email(output, index, transaction, to_emails, **kwargs):
+    body = "El output " + output + u" con índice " + index + u" fue utilizado en la transacción " + transaction + "."
+    email = EmailMessage("Uno de los outputs que estabas siguiendo fue utilizado", body,
                          settings.EMAIL_HOST_USER,
                          to_emails, **kwargs)
-
     email.content_subtype = 'html'
     email.send()
